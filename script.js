f69eaf1e8d45e052ce068a190f5ee2d679b12e30
ishaq <ishaqdz09@gmail.com>
2024-05-04 18:09:10 +0100

    chwi java

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//
+function imagetopage(){
+    documen.getElemntByID("slide1")
+}
+
+// Get the link element by its ID
+var alertLink1 = document.getElementById('offercon');
+var alertLink2 = document.getElementById('supcon');
+// Add a click event listener to the link
+alertLink1.addEventListener('click', function(event) {
+  
+    // Prevent the default action of the link
+  event.preventDefault();
+
+  // Show an alert when the link is clicked
+  alert('this page is under construction!!!');
+});
+alertLink2.addEventListener('click', function(event) {
+  
+    // Prevent the default action of the link
+  event.preventDefault();
+
+  // Show an alert when the link is clicked
+  alert('this page is under construction!!!');
+});
